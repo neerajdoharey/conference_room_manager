7e0ce17dfe317e70765fa50c33e2df766b3c696c
Neeraj Doharey <neerajlivewire@gmail.com>
2018-10-06 04:46:34 +0530

    Change in user table

--- db/migrate/20180929083659_add_first_and_last_name_to_user.rb
+++ db/migrate/20180929083659_add_first_and_last_name_to_user.rb
@@ -2,6 +2,6 @@ class AddFirstAndLastNameToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :phone_no, :integer
+    add_column :users, :phone_no, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_042540) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone_no"
+    t.string "phone_no"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
